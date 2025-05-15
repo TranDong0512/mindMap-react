47c04cee9a3481e76dd080376cf1ef371fc9665e
TranDong0512 <tdong0512@gmail.com>
2025-05-15 10:00:26 +0700

    commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import { Counter } from './code/HOC'
 function App() {
   const router = createBrowserRouter([...publicRouter])
   return (
-    // <RouterProvider router={router} />
-    <Counter />
+    <RouterProvider router={router} />
+    // <Counter />
   )
 }
 
